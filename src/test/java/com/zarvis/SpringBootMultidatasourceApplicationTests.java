35a163c28dc087b22c0a0e7a49b648023bbaa829
Sambhav Khare <sambhav.khare92@gmail.com>
2019-08-27 21:21:57 +0530

    initial commit

---
+++ src/test/java/com/zarvis/SpringBootMultidatasourceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zarvis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMultidatasourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
